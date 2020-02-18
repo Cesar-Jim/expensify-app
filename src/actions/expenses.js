7977afe66592f2544332fa65d09accd10779a111
Cesar Jimenez <cijgr@hotmail.com>
2020-02-18 13:14:38 -0600

    setup fetching expenses from firebase. Check why async tests are not working

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,5 +1,6 @@
 import uuid from 'uuid';
 import dataBase from '../firebase/firebase';
+import firebase, { database } from 'firebase';
 
 // Before Firebase:
 // component calls action generator
@@ -57,3 +58,28 @@ export const editExpense = (id, updates) => ({
   id,
   updates,
 });
+
+// SET_EXPENSES
+export const setExpenses = expenses => ({
+  type: 'SET_EXPENSES',
+  expenses,
+});
+
+export const startSetExpenses = () => {
+  return dispatch => {
+    const db = firebase.database().ref('expenses');
+
+    return db.once('value').then(snapshot => {
+      const expenses = [];
+
+      snapshot.forEach(childSnapshot => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val(),
+        });
+      });
+
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
